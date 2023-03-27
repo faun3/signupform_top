bafc3bdbc4dca704d5a915e900a2d462bb340076
faun3 <antonioculcea@gmail.com>
2023-03-27 21:25:03 +0300

    write password checker logic

--- validate.js
+++ validate.js
@@ -1,1 +1,18 @@
 console.log('Hi')
+const form = document.querySelector('form');
+const pass = document.querySelector('#pass');
+const passConfirm = document.querySelector('input#passconfirm');
+
+const submitButton = document.querySelector('.buttonWrapper > button');
+submitButton.addEventListener('input', (event) => {
+    let p1 = form.pass.value;
+    let p2 = form.passconfirm.value;
+    event.preventDefault;
+    if (p1 !== p2){
+        passConfirm.classList.toggle('error');
+    } 
+    else{
+        passConfirm.classList.toggle('error');
+        form.submit();
+    }
+});
